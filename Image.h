9f3b290033321613b10e8925d0be68ebf5dcfdf0
Сергей Леонтьев <Sergey130702@mail.ru>
2023-02-14 12:59:25 +0400

    upgrade

--- Image.h
+++ Image.h
@@ -1,6 +1,5 @@
 #include<iostream>
-#include<cstdio>
-#include<stdexcept>
+#include<iterator>
 #include<vector>
 using namespace std;
 
@@ -9,71 +8,53 @@ class Image {
 private:
 	vector<vector<T>> image;
 	size_t width, height;
-public:
-	struct proxy {
-	private:
-		T* ptr;
-	public:
-		proxy(T* ptr) : ptr(ptr) {}
-
-
-
-		proxy& operator=(const T& value) {
-			*ptr = value;
-			return *this;
-		}
-
-		operator T() const {
-			return *ptr;
-		}
-	};
-
-	void get_roi(const Image& a_image, size_t& x_min, size_t& y_min, size_t& result_width, size_t& result_height)
-	{
-		if (width > a_image.width) {
-			result_width = width;
-			x_min = a_image.width;
-		}
-		else
+public:	
+		void get_roi(const Image& a_image, size_t& x_min, size_t& y_min, size_t& result_width, size_t& result_height)
 		{
-			result_width = a_image.width;
-			x_min = width;
+			if (width > a_image.width) {
+				result_width = width;
+				x_min = a_image.width;
+			}
+			else
+			{
+				result_width = a_image.width;
+				x_min = width;
+			}
+			if (height > a_image.height) {
+				result_height = height;
+				y_min = a_image.height;
+			}
+			else
+			{
+				result_height = a_image.height;
+				y_min = height;
+			}
 		}
-		if (height > a_image.height) {
-			result_height = height;
-			y_min = a_image.height;
-		}
-		else
-		{
-			result_height = a_image.height;
-			y_min = height;
-		}
-	}
 
-	void copy_outside_roi(const Image& a_image, size_t& x_min, size_t& y_min, size_t& result_width, size_t& result_height, size_t& result_image)
-	{
-		if (width > a_image.width || width < a_image.width) {
-			for (size_t i = x_min; i < result_width; ++i) {
-				for (size_t j = 0; j < result_height; ++j) {
-					result_image.image[i][j] = image[i][j];
+		void copy_outside_roi(const Image& a_image, size_t& x_min, size_t& y_min, size_t& result_width, size_t& result_height, size_t& result_image)
+		{
+			if (width > a_image.width || width < a_image.width) {
+				for (size_t i = x_min; i < result_width; ++i) {
+					for (size_t j = 0; j < result_height; ++j) {
+						result_image.image[i][j] = image[i][j];
+					}
+				}
+			}
+			if (height > a_image.height || height < a_image.height) {
+				for (size_t i = 0; i < x_min; ++i) {
+					for (size_t j = y_min; j < result_height; ++j) {
+						result_image.image[i][j] = image[i][j];
+					}
 				}
 			}
 		}
-		if (height > a_image.height || height < a_image.height) {
-			for (size_t i = 0; i < x_min; ++i) {
-				for (size_t j = y_min; j < result_height; ++j) {
-					result_image.image[i][j] = image[i][j];
-				}
-			}
-		}
-	}
 
 	Image();
 	Image(const size_t a_width, const size_t a_height);
 	Image(const Image& image_copy);
 	~Image();
-	proxy operator()(const size_t a_width, const size_t a_height);
-	proxy operator()(const size_t a_width, const size_t a_height) const;
+	T operator()(const size_t a_width, const size_t a_height) const;
+	T& operator()(const size_t a_width, const size_t a_height);
 	Image operator =(const Image& a_image);
 	Image operator *(const Image& a_image) const;
 	Image operator +(const Image& a_image) const;
@@ -82,15 +63,16 @@ public:
 	//Image operator !() const;
 	bool operator ==(const Image& a_image) const;
 	double ratio() const;
+	typename vector<vector<T>>::iterator begin() const;
+	typename vector<vector<T>>::iterator end() const;
 	size_t get_width() const;
 	size_t get_height() const;
-	auto begin() const;
-	auto end() const;
+	void print_im() const;
 };
 
 
 template <typename T>
-Image<T>::Image() : image(nullptr), width(0), height(0) {}
+Image<T>::Image() : image(0), width(0), height(0) {}
 
 template <typename T>
 Image<T>::Image(const size_t a_width, const size_t a_height) {
@@ -99,35 +81,37 @@ Image<T>::Image(const size_t a_width, const size_t a_height) {
 	}
 	width = a_width;
 	height = a_height;
-	image = vector<vector<T>>(width, vector<T>(height));
+	for (size_t i = 0; i < width; ++i) {
+		image.push_back(vector<T>(height));
+	}
 }
 
 template <typename T>
 Image<T>::Image(const Image& image_copy) {
 	width = image_copy.width;
 	height = image_copy.height;
-	image = vector<vector<T>>(width, vector<T>(height));
+	image.resize(width);
 	for (size_t i = 0; i < width; i++)
 	{
 		for (size_t j = 0; j < height; j++)
 		{
-			image[i][j] = image_copy.image[i][j];
+			image[i].push_back(image_copy(i, j));
 		}
 	}
 }
 
 template <typename T>
-typename Image<T>::proxy Image<T>::operator()(const size_t a_width, const size_t a_height) {
-	if (a_width > width - 1 || a_height > height - 1)
+T Image<T>::operator()(const size_t a_width, const size_t a_height) const {
+	if (a_width > width || a_height > height)
 		throw std::out_of_range( "Incorrect coordinates" );
-	return proxy(&image[a_width][a_height]);
+	return image[a_width][a_height];
 }
 
 template <typename T>
-typename Image<T>::proxy Image<T>::operator()(const size_t a_width, const size_t a_height) const {
-	if (a_width > width - 1 || a_height > height - 1)
+T& Image<T>::operator()(const size_t a_width, const size_t a_height) {
+	if (a_width > width || a_height > height)
 		throw std::out_of_range( "Incorrect coordinates" );
-	return proxy(&image[a_width][a_height]);
+	return image[a_width][a_height];
 }
 
 template <typename T>
@@ -135,21 +119,14 @@ Image<T> Image<T>::operator =(const Image& a_image) {
 	width = a_image.width;
 	height = a_image.height;
 	if (width != a_image.width || height != a_image.height) {
-		if (image.empty()) {
+		if (image) {
 			image.clear();
 		}
-		else
-		{
-			for (auto i : image) {
-				i.clear();
-			}
-			image.clear();
-		}
-		image = vector<vector<T>>(width, vector<T>(height));
+		image.resize(width);
 	}
 	for (size_t i = 0; i < width; ++i) {
 		for (size_t j = 0; j < height; ++j) {
-			image[i][j] = a_image.image[i][j];
+			image[i].push_back(a_image(i, j));
 		}
 	}
 	return a_image;
@@ -235,7 +212,8 @@ Image<T> Image<T>::operator +(const Image& a_image) const {
 		y_min = height;
 	}
 	Image<T> result_image(result_width, result_height);
-	for (size_t i = 0; i < x_min; ++i) {
+	for (size_t i = 0; i < x_min; ++i) 
+	{
 		for (size_t j = 0; j < y_min; ++j) {
 			result_image.image[i][j] = image[i][j] + a_image.image[i][j];
 		}
@@ -355,6 +333,16 @@ double Image<T>::ratio() const {
 }
 
 template <typename T>
+typename vector <vector<T>>::iterator Image<T>::begin() const {
+	return image[0].begin();
+}
+
+template <typename T>
+typename vector<vector<T>>::iterator Image<T>::end() const {
+	return image[width - 1].end();
+}
+
+template <typename T>
 size_t Image<T>::get_width() const {
 	return width;
 }
@@ -364,26 +352,16 @@ size_t Image<T>::get_height() const {
 	return height;
 }
 
-template<typename T>
-auto Image<T>::begin() const {
-	return image.begin();
-}
-
-template<typename T>
-auto Image<T>::end() const {
-	return image.end();
-}
+template <typename T>
+Image<T>::~Image<T>() {}
 
 template <typename T>
-Image<T>::~Image<T>() {
-	if (image.empty()) {
-		image.clear();
-	}
-	else
+void Image<T>::print_im()const
+{
+	for (auto it : image)
 	{
-		for (auto i : image) {
-			i.clear();
-		}
-		image.clear();
+		for (auto iter : it)
+			cout << "\t" << iter;
+		cout << endl;
 	}
 }
